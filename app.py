2e7a87060e625d31bb6e4583a9be5a8f1fc96ad9
Surendra <maharjan@chapman.edu>
2025-02-15 19:23:30 -0800

    Added new file

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",methods=['GET'])
+@app.route("/", methods=['GET'])
 def home():
-    return "<h1>Arpan's Flask App</h1>"
+    return """
+    <html>
+        <head>
+            <title>Arpan's Flask App</title>
+        </head>
+        <body style="text-align: center; margin-top: 50px;">
+            <h1>Arpan's Flask App</h1>
+            <button onclick="alert('Button Clicked!')">Click Me</button>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
